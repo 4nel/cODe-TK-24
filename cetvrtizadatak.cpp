584301949b59541acf81ab84aa7f186674a32529
anel <105921696+4nel@users.noreply.github.com>
2023-12-09 15:04:47 +0100

    Updated cetvrtizadatak.cpp
    Ups moglo je bolje

--- cetvrtizadatak.cpp
+++ cetvrtizadatak.cpp
@@ -18,24 +18,15 @@ int main()
       s[i]='\n';
       continue;
     }
-    if(s[i]=='b'){
+    if(s[i]=='b'||s[i]=='B'){
       for(int j=i-1; j>=0; j--){
-        if(s[j]<='z'&&s[j]>='a'){
+        if(s[j]<=s[i]+24&&s[j]>=s[i]-1){
           s[j]='\n';
           break;
         }
       }
       s[i]='\n';
       continue;
-    }
-    if(s[i]=='B'){
-      for(int j=i-1; j>=0; j--){
-        if(s[j]<='Z'&&s[j]>='A'){
-          s[j]='\n';
-          break;
-        }
-      }
-      s[i]='\n';
     }
   }
   for (const char c: s){
