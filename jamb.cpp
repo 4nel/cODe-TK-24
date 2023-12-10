6bf80b652eb17d364eb3e7c0e7e6fd12cf6ac0b1
anel <105921696+4nel@users.noreply.github.com>
2023-12-10 22:36:36 +0100

    Updated jamb.cpp
    hvala azra

--- jamb.cpp
+++ jamb.cpp
@@ -13,9 +13,9 @@ int main()
 
   int n;
   cin>>n;
-  for(int i=1; i<n; i++){
-    for(int j=1; j<n; j++){
-      if(i+j<n)cout<<i<<' '<<j<<' '<<n-i-j<<'\n';
+  for(int i=1; i<=6; i++){
+    for(int j=1; j<=6; j++){
+      if(n-i-j<=6)cout<<i<<' '<<j<<' '<<n-i-j<<'\n';
     }
   }
 
